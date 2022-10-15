6e6674d32a2bfae3f90295db059357ad4a84ef19
JAYRAJARORA <jayraj.arora@gmail.com>
2022-10-14 18:40:03 -0700

    Reformatted utility function and added comments

--- DistanceNode.java
+++ DistanceNode.java
@@ -1,54 +1,22 @@
-import java.util.ArrayList;
-
+/**
+ * Class representing a doubly linked list
+ * A node contains a city name, the distance to reach that city, next and previous pointers to link the nodes
+ */
 public class DistanceNode {
-    char node;
+    char city;
     int distance;
     DistanceNode previous;
     DistanceNode next;
 
-    DistanceNode(char node, int distance) {
-        this.node = node;
+    /**
+     * Constructor to create a node
+     * @param city city name eg. A, B, C
+     * @param distance distance to reach a node e.g. 90, 60, 70
+     */
+    DistanceNode(char city, int distance) {
+        this.city = city;
         this.distance = distance;
         previous = null;
         next = null;
-    }
-
-    public static ArrayList<Character> minimizeStops(int carRange, int n, DistanceNode head) {
-        DistanceNode curNode = head;
-        // will contain the list of minimum stops required
-        ArrayList<Character> stops = new ArrayList<>();
-
-        // first stop will always be in the list - recharging the car at distance 0
-        stops.add(curNode.node);
-
-        int currentStopDistance = 0;
-
-        while(curNode != null) {
-            // calculating the total distance travelled so far on a single range, after every recharge it becomes zero
-            int distanceTravelledSoFar = 0;
-
-            // iterating till we get to the last node or total distance covered + currentDistance exceeds the car range
-            do {
-                curNode = curNode.next;
-                currentStopDistance = curNode.distance;
-                distanceTravelledSoFar += currentStopDistance;
-            } while (curNode.next != null && distanceTravelledSoFar + currentStopDistance < carRange);
-
-            // checking if we need to make a stop at previous station
-            if (distanceTravelledSoFar + currentStopDistance > carRange) {
-                // go to previous node and add it to the stop where car needs to be fully recharged
-                curNode = curNode.previous;
-                stops.add(curNode.node);
-            }
-            // if it's a last node in the list, add it as this is the destination stop
-            if(curNode.next == null) {
-                stops.add(curNode.node);
-
-                return stops;
-            }
-
-        }
-        
-        return stops;
     }
 }
--- ElectricCarTraveller.java
+++ ElectricCarTraveller.java
@@ -3,9 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * Reads the inputs(number of cities(n), car range(C), and distances between cities) and
+ * calls the helper function that minimizes the stops between cities
+ */
 public class ElectricCarTraveller {
-    // nodes representing maximum number of cities(19 as cities are less than 20)
-    static char nodes[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'};
+    /**
+     * nodes in a graph representing the cities
+     * (max 19 nodes represented based on assumption that cities are less than 20)
+     */
+    static char[] cities = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S'};
+
+    /**
+     * Main function that is called when the program is run.
+     * It read inputs, prepares a doubly linked list, calls the utility function minimizeStops
+     * and prints the ArrayList returned
+     *
+     * @see DistanceNode
+     * @param args string arguments
+     * @throws IOException exception to be caught when taking an input from input stream
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -15,38 +33,80 @@ public class ElectricCarTraveller {
         System.out.print("Please enter the number of cities travelled by electric car: ");
         int n = Integer.parseInt(reader.readLine());
 
-        // n-1 distances taken as n cities will have n-1 distances between them
+        // n-1 distances taken as n cities will have n-1 distances between them, distances are edges between cities
         int[] distances = new int[n-1];
-        System.out.println("Please enter the list of distances between the cities: ");
 
+        System.out.println("Please enter the list of distances between the cities: ");
+        // reading distances between cities from the input stream
         for(int i = 0; i < n-1; i++) {
-            System.out.print("Please enter the distance from " + nodes[i] + " -> " + nodes[i+1] + ": ");
+            System.out.print("Please enter the distance from " + cities[i] + " -> " + cities[i+1] + ": ");
             distances[i] = Integer.parseInt(reader.readLine());
         }
+        // preparing the linked list, adding the first node (A | 0)
+        DistanceNode head = new DistanceNode(cities[0], 0);
 
-        DistanceNode head = new DistanceNode(nodes[0], 0);
+        // pointer that points to the current node and links new pointers
         DistanceNode cur = head;
-        for(int i=1; i<n; i++) {
-            // add characters and its weight to generate a weighted graph
-            DistanceNode newNode = new DistanceNode(nodes[i], distances[i-1]);
 
+        for(int i=1; i<n; i++) {
+            // adding cities and its weights to generate a weighted graph
+            DistanceNode newNode = new DistanceNode(cities[i], distances[i-1]);
+
+            // linking nodes
             cur.next = newNode;
             newNode.previous = cur;
+            // making the new node as current code
             cur = cur.next;
         }
 
-        printWeightedGraph(head);
-
-        ArrayList<Character> stops = DistanceNode.minimizeStops(C, n, head);
-        System.out.println("Minimal number of stops is : " + stops.toString());
+        // returned ArrayList of stops
+        ArrayList<Character> stops = minimizeStops(C, head);
+        System.out.println("Minimal number of stops is : " + stops);
     }
 
-    public static void printWeightedGraph(DistanceNode head) {
-        DistanceNode cur = head;
-        System.out.println("Printing weight matrix: ");
-        while(cur!=null) {
-            System.out.println("Character : " + cur.node + " Distance: " + cur.distance);
-            cur = cur.next;
+    /**
+     * Iterates over the nodes, calculating the distance travelled so far
+     * and checking if we need to make a stop at previous station
+     * Appends the current city to the ArrayList if stop is required
+     *
+     * @param carRange Capacity of the car taken from input
+     * @param head Pointer to first node in the weighted graph(linked list)
+     *             eg. A | 0 <-> B | 90 <-> C | 60 <-> D | 70 <-> E | 65 <-> F |  83 <-> G | 75 <-> H | 72
+     * @return stops List of minimum stops required
+     */
+    public static ArrayList<Character> minimizeStops(int carRange, DistanceNode head) {
+        DistanceNode curNode = head;
+        // will contain the list of minimum stops required
+        ArrayList<Character> stops = new ArrayList<>();
+
+        // first stop will always be in the list - recharging the car at distance 0
+        stops.add(curNode.city);
+        // distance to reach the current node
+        int currentStopDistance;
+
+        while(curNode.next != null) {
+            // calculating the total distance travelled so far on a single range; after every recharge it becomes zero
+            int distanceTravelledSoFar = 0;
+
+            // iterating till we get to the last node or total distance covered + currentDistance exceeds the car range
+            do {
+                curNode = curNode.next;
+                // current distance updated
+                currentStopDistance = curNode.distance;
+                // adding the current distance to total distance travelled so far
+                distanceTravelledSoFar += currentStopDistance;
+            } while (curNode.next != null && distanceTravelledSoFar + currentStopDistance < carRange);
+
+            // checking if we need to make a stop at previous station
+            if (distanceTravelledSoFar + currentStopDistance > carRange) {
+                // go to previous node and add it to the stop where car needs to be fully recharged
+                curNode = curNode.previous;
+                stops.add(curNode.city);
+            }
         }
+        // add the last node in the list as it is the destination stop
+        stops.add(curNode.city);
+
+        return stops;
     }
 }
